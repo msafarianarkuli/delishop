2022d590fbe6e2503b71285143015a5fc121b26b
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-16 20:45:57 +0330

    added max width

--- src/template/index.tsx
+++ src/template/index.tsx
@@ -8,7 +8,9 @@ interface ITemplate {
 function Template({children}: ITemplate) {
   return (
     <AntConfig>
-      <div className="template min-h-screen anti aliased font-IranSans text-[14px] text-textColor">{children}</div>
+      <div className="template min-h-screen anti aliased font-IranSans text-[14px] text-textColor">
+        <div className="max-w-[700px] mx-auto">{children}</div>
+      </div>
     </AntConfig>
   );
 }

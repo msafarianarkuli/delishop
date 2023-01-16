72165582fcad631e40bc92e1d113a6d43e1207e9
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-16 12:50:33 +0330

    added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   important: true,
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF5C01",
+      },
+    },
   },
   plugins: [],
 };

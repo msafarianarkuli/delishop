a8066a7e5a53a9282ca70e824b101e7b1a7a3cb8
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-18 12:10:52 +0330

    added icon

---
+++ src/assets/icons/IconSort.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function IconSort({className}: {className: string}) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 15 13"
+      width="15"
+      height="13"
+      fill="currentColor"
+    >
+      <path d="M8.1 7.65H.9a.9.9 0 1 0 0 1.8h7.2a.9.9 0 0 0 0-1.8ZM14.13 8.37a.87.87 0 0 0-1.26 0l-.72.675V.9a.9.9 0 1 0-1.8 0v10.215a.746.746 0 0 0 .27.72.87.87 0 0 0 1.26 0l2.205-2.205a.912.912 0 0 0 .045-1.26ZM8.1.45H.9a.9.9 0 1 0 0 1.8h7.2a.9.9 0 0 0 0-1.8ZM.9 5.85h6.3a.9.9 0 0 0 0-1.8H.9a.9.9 0 1 0 0 1.8Z" />
+    </svg>
+  );
+}
+
+export default IconSort;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,7 +2,9 @@ import IconRoundedRight from "assets/icons/IconRoundedRight";
 import IconMenu from "assets/icons/IconMenu";
 import IconArrowDown from "assets/icons/IconArrowDown";
 import IconCoin from "assets/icons/IconCoin";
+import IconCategory from "assets/icons/IconCategory";
+import IconSort from "assets/icons/IconSort";
 
 export type TIcons = ({className}: {className: string}) => JSX.Element;
 
-export {IconRoundedRight, IconMenu, IconArrowDown, IconCoin};
+export {IconRoundedRight, IconMenu, IconArrowDown, IconCoin, IconCategory, IconSort};

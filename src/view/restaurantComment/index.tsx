249e3eec0083e58c4e71248ab44caa4be7854338
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-22 00:00:18 +0330

    added restaurant comment items

--- src/view/restaurantComment/index.tsx
+++ src/view/restaurantComment/index.tsx
@@ -1,5 +1,8 @@
 import RestaurantCommentHeader from "view/restaurantComment/component/RestaurantCommentHeader";
 import RestaurantCommentRate from "view/restaurantComment/component/RestaurantCommentRate";
+import RestaurantCommentItem from "view/restaurantComment/component/RestaurantCommentItem";
+
+const arr = Array.from(new Array(5), (_, i) => i + 1);
 
 function RestaurantComment() {
   return (
@@ -8,6 +11,19 @@ function RestaurantComment() {
       <div className="mt-headerNormal px-screenSpace">
         <RestaurantCommentRate />
         <div className="my-5 text-[18px] font-medium">نظرات کاربران</div>
+        {arr.map((item) => {
+          return (
+            <div key={item} className="mb-5">
+              <RestaurantCommentItem
+                name="الناز"
+                date="5 مهر 1401"
+                star={4}
+                tag="خوراک کباب کوبیده"
+                answer="ضمن عرض پوزش ازشما به اطلاع می رساند غذاهای ارائه شده توسط مجموعه تحت نظارت واحد کنترل کیفیت بوده و وزن و رسپی آن مطابق مقادیر اعلام شده می باشد. با سپاس از همراهی شما"
+              />
+            </div>
+          );
+        })}
       </div>
     </>
   );

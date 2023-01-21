cc1e7dfc5139b8c2b294f6895954f6acd458b487
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-21 19:02:23 +0330

    added back in restaurantdetail

--- src/view/restaurantDetail/component/RestaurantDetailHeader.tsx
+++ src/view/restaurantDetail/component/RestaurantDetailHeader.tsx
@@ -1,11 +1,13 @@
 import {AppHeader, BackBtn} from "components";
 import IconFavoriteOutline from "assets/icons/IconFavoriteOutline";
+import {useRouter} from "next/router";
 
 function RestaurantDetailHeader() {
+  const router = useRouter();
   return (
     <div className="fixed top-0 right-0 left-0 z-10">
       <AppHeader
-        right={<BackBtn />}
+        right={<BackBtn onClick={() => router.back()} />}
         left={
           <button>
             <IconFavoriteOutline className="w-6 h-6 text-white" />

73825f0a0b3933c1a854ffa578f4be72a8afa4ff
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-22 13:38:18 +0330

    change bottom navigation background

--- src/components/bottomNavigation/BottomNavigation.tsx
+++ src/components/bottomNavigation/BottomNavigation.tsx
@@ -22,7 +22,7 @@ const data = [
 
 function BottomNavigation() {
   return (
-    <div className="fixed flex items-center justify-around bottom-0 right-0 left-0 h-bottomNavigation bg-white">
+    <div className="fixed flex items-center justify-around bottom-0 right-0 left-0 h-bottomNavigation bg-[#E1E2E8]">
       {data.map((item, index) => {
         const icon = classNames({
           "w-6 h-6": true,

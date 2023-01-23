225c14674010a0be2461a94084ab6d84deca54e5
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-23 15:00:31 +0330

    created appTabRoute

---
+++ src/components/appTabRoute/AppTabRoute.tsx
@@ -0,0 +1,41 @@
+import classNames from "classnames";
+import Link from "next/link";
+import styles from "components/appTabRoute/appTabRoute.module.scss";
+
+export type TAppTabRoutes = {link: string; title: string; active: boolean}[];
+
+interface IAppTabRoute {
+  routes: TAppTabRoutes;
+  classNameContainer?: string;
+  classNameItem?: string;
+  classNameItemActive?: string;
+}
+
+function AppTabRoute(props: IAppTabRoute) {
+  const {routes, classNameContainer = "", classNameItem = "", classNameItemActive = ""} = props;
+  const container = classNames({
+    "flex w-full items-center h-headerNormal": true,
+    [classNameContainer]: classNameContainer,
+  });
+  return (
+    <div className={container}>
+      {routes.map((item, index) => {
+        const className = classNames({
+          "flex items-center justify-center h-[40px] rounded-full font-medium": true,
+          [styles.app_tab_route_active]: item.active,
+          "text-textColorLight": !item.active,
+          "text-primary": item.active && !classNameItemActive,
+          [classNameItem]: classNameItem,
+          [classNameItemActive]: classNameItemActive && item.active,
+        });
+        return (
+          <Link key={index} className={className} href={item.link}>
+            {item.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default AppTabRoute;
--- src/view/awardReceived/index.tsx
+++ src/view/awardReceived/index.tsx
@@ -1,9 +1,11 @@
 import AwardReceivedHeader from "view/awardReceived/component/AwardReceivedHeader";
+import AwardReceivedTab from "view/awardReceived/component/AwardReceivedTab";
 
 function AwardReceived() {
   return (
     <>
       <AwardReceivedHeader />
+      <AwardReceivedTab />
     </>
   );
 }

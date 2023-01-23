bdc2ca83bcb9bc128798f77230997e2c853056f1
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-01-23 17:53:49 +0330

    show list in awardReceived

--- src/view/awardReceived/index.tsx
+++ src/view/awardReceived/index.tsx
@@ -2,13 +2,24 @@ import {WalletTabRoute} from "components";
 import WalletAppHeader from "components/appHeader/component/WalletAppHeader";
 import AwardReceivedCard from "view/awardReceived/component/AwardReceivedCard";
 
+const arr = Array.from(new Array(10), (_, i) => i + 1);
+
 function AwardReceived() {
   return (
     <>
       <WalletAppHeader />
       <WalletTabRoute active="awardReceived" />
       <div className="px-screenSpace mt-[132px]">
-        <AwardReceivedCard />
+        {arr.map((item) => {
+          return (
+            <AwardReceivedCard
+              key={item}
+              title="سفارش غذا از اپلیکیشن آریایی"
+              discount="NSH-RM-21"
+              description="50 هزار تومان تخفیف سفارش غذا"
+            />
+          );
+        })}
       </div>
     </>
   );

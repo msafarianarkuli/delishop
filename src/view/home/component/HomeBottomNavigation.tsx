3e565fcdaf609ff13dee957ffbf392ab1a412f9a
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-02-03 11:59:55 +0330

    fixed home page bottom navigation

--- src/view/home/component/HomeBottomNavigation.tsx
+++ src/view/home/component/HomeBottomNavigation.tsx
@@ -1,19 +1,30 @@
 import styles from "view/home/home.module.scss";
+import message from "assets/images/homeMessage.svg";
+import divar from "assets/images/homeDivar.svg";
+import game1 from "assets/images/homeGame.svg";
+import game2 from "assets/images/homeGame2.svg";
 
-const data = ["دیوار", "پیام ها", "aa", "crash"];
+const data = [
+  {title: "پیام ها", image: message.src},
+  {title: "دیوار", image: divar.src},
+  {title: "بازی 1", image: game1.src},
+  {title: "بازی 2", image: game2.src},
+];
 
 function HomeBottomNavigation() {
   return (
     <div className="fixed z-10 bottom-[20px] left-[44px] right-[44px]">
       <div className={styles.home_bottom_navigation}>
-        {data.map((item, index) => {
-          return (
-            <div key={index} className="ml-5 last:ml-0">
-              <div className="w-[55px] h-[55px] rounded-[6px] bg-[#D9D9D9]" />
-              <div className="text-[13px] text-center mt-1">{item}</div>
-            </div>
-          );
-        })}
+        <div className="flex w-full items-center justify-between mobile:mx-[30px] mx-3">
+          {data.map((item, index) => {
+            return (
+              <div key={index} className="h-[78px] w-full ml-2 last:ml-0">
+                <img src={item.image} alt={item.title} className="w-auto h-auto rounded-[6px] mx-auto" />
+                <div className="text-[13px] text-center mt-1">{item.title}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        mobile: "360px",
+        sm: "576px",
+        md: "768px",
+        lg: "900px",
+        xl: "1200px",
+      },
       fontFamily: {
         IranSans: [
           "IRANSans",

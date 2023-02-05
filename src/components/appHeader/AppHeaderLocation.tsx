4bb896d2d15ec16eba8ce6d813a7284042c2f2c3
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-02-05 17:47:15 +0330

    added link to AppHeaderLocation

--- src/components/appHeader/AppHeaderLocation.tsx
+++ src/components/appHeader/AppHeaderLocation.tsx
@@ -1,14 +1,15 @@
 import {IconArrowDown} from "assets/icons";
+import Link from "next/link";
 
 function AppHeaderLocation() {
   return (
-    <div className="text-[14px]">
+    <Link href="/address" className="block text-[14px]">
       <div className="flex items-center justify-center">
         <span className="font-bold">خانه</span>
         <IconArrowDown className="w-3 h-auto text-iconColor mr-1" />
       </div>
       <div className="text-[13px] font-light max-w-[130px] truncate">کشاورز، پارک لاله، کارگر جنوبی</div>
-    </div>
+    </Link>
   );
 }
 

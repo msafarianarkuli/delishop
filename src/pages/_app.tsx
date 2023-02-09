c139085fe0fdefc584c2201153ae9a50aaa39965
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-02-09 14:13:46 +0330

    fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ function App({Component, ...rest}: AppProps) {
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable='no'" />
         <title>دلی شاپ</title>
       </Head>
       <QueryClientProvider client={queryClient}>

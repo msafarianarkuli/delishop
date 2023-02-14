f777c0c47ea8e81123388db0f16d75194e802f7c
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-02-14 13:10:02 +0330

    fixed font-size

--- src/view/restaurantProductDetail/component/RestaurantProductDetailDescription.tsx
+++ src/view/restaurantProductDetail/component/RestaurantProductDetailDescription.tsx
@@ -1,6 +1,6 @@
 function RestaurantProductDetailDescription() {
   return (
-    <div className="text-[15px] font-medium px-screenSpace">
+    <div className="text-[13px] font-medium px-screenSpace">
       خمیر پیتزا کلاسیک، هات داگ گوشت تازه ۹۰%، سالامی، هات داگ گودا، پنیر موزارلا، فلفل دلمه‌ای، سس پنیر گودا، قارچ،
       گوجه، کنجد
     </div>

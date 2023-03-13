0c94082782fe03ab2127c1d52383c4f80895cb04
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-03-13 22:10:58 +0330

    added initial page

---
+++ src/pages/supermarket/cart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SupermarketCartPage() {
+  return <div>SupermarketCartPage</div>;
+}
+
+export default SupermarketCartPage;
---
+++ src/pages/supermarket/search.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SupermarketSearchPage() {
+  return <div>SupermarketSearchPage</div>;
+}
+
+export default SupermarketSearchPage;

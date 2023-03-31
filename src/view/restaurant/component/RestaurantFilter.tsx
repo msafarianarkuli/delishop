3acf1b3843a97d581e5537efcc5d541800125868
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-03-31 18:02:28 +0330

    fixed restaurant filter padding

--- src/view/restaurant/component/RestaurantFilter.tsx
+++ src/view/restaurant/component/RestaurantFilter.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import RestaurantCategory from "view/restaurant/component/RestaurantCategory";
 import RestaurantFilterBtn from "view/restaurant/component/RestaurantFilterBtn";
 import RestaurantFilterBottomSheet from "view/restaurant/component/RestaurantFilterBottomSheet";
@@ -7,6 +7,7 @@ import getVendorsTags, {VENDORS_TAGS_KEY} from "api/getVendorsTags";
 import usePathnameQuery from "hooks/usePathnameQuery";
 import {useRouter} from "next/router";
 import {restaurantTagQuery} from "view/restaurant/context/RestaurantDataProvider";
+import classNames from "classnames";
 
 // export const restaurantTagQuery = "tag[]";
 const defaultTags = ["کباب", "ایرانی", "دارای سکه"];
@@ -68,11 +69,16 @@ function RestaurantFilter() {
     [allTagValues, onAdd, onRemove, push]
   );
 
+  const containerClassName = classNames({
+    "flex items-center px-screenSpace max-width-screen": true,
+    "py-4": !constTags.length && !selectedFilter.length,
+  });
+
   return (
     <>
-      <div className="flex items-center px-screenSpace max-width-screen py-5">
+      <div className={containerClassName}>
         <RestaurantCategory count={allTagValues.length} onClick={() => setBottomSheet(true)} />
-        <div className="flex flex-1 flex-nowrap overflow-auto px-2">
+        <div className="flex flex-1 flex-nowrap overflow-auto px-2 py-5">
           {constTags.map((item) => {
             return (
               <RestaurantFilterBtn

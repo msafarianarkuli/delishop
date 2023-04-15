be603077e3a7c2bbb3cdf92489c820f21822b325
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-04-15 15:40:36 +0330

    fixed bug in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/profile/:path*", "/address", "/address/create/:path*", "/order/:path*"],
+  matcher: ["/profile/:path*", "/address", "/address/create/:path*", "/restaurant/order/:path*"],
 };

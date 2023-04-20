ec9683b65e24c2b3637404a6b6fbf808d31f1b9a
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-04-20 14:17:40 +0330

    added pagination for pervious and active order

--- src/view/restaurantOrderActive/component/RestaurantOrderActiveList.tsx
+++ src/view/restaurantOrderActive/component/RestaurantOrderActiveList.tsx
@@ -1,6 +1,8 @@
 import RestaurantOrderActiveCard from "view/restaurantOrderActive/component/restaurantOrderActiveCard/RestaurantOrderActiveCard";
 import {useRouter} from "next/router";
 import {useRestaurantOrderActiveData} from "view/restaurantOrderActive/context/RestaurantOrderActiveDataProvider";
+import {useInView} from "react-intersection-observer";
+import {useEffect} from "react";
 
 function RestaurantOrderActiveList() {
   const {data, isLoading} = useRestaurantOrderActiveData();
@@ -15,27 +17,37 @@ function RestaurantOrderActiveList() {
 
 function RestaurantOrderActiveListShow() {
   const router = useRouter();
-  const {data} = useRestaurantOrderActiveData();
+  const {data, fetchNextPage} = useRestaurantOrderActiveData();
+  const {ref, inView} = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   return (
     <>
-      {data?.pages.map((value) => {
-        return value.orders.map((item) => {
+      {data?.pages.map((value, index, array) => {
+        return value.orders.map((item, idx, arr) => {
+          const condition = array.length - 1 === index && arr.length - 1 === idx;
+          const tmpRef = condition ? ref : null;
           return (
-            <RestaurantOrderActiveCard
-              key={item.id}
-              title={item.vendor.name}
-              image={item.vendor.logo}
-              deliveryTitle={item.address.title || ""}
-              date={item.created_at}
-              coin={15}
-              receiptNumber={item.id}
-              deliveryTime={"15:00"}
-              totalPrice={Math.round(item.topayprice / 10)}
-              onClickSubmit={() => {
-                router.push(`/restaurant/order/${item.id}`);
-              }}
-            />
+            <div ref={tmpRef} key={item.id}>
+              <RestaurantOrderActiveCard
+                title={item.vendor.name}
+                image={item.vendor.logo}
+                deliveryTitle={item.address.title || ""}
+                date={item.created_at}
+                coin={15}
+                receiptNumber={item.id}
+                deliveryTime={"15:00"}
+                totalPrice={Math.round(item.topayprice / 10)}
+                onClickSubmit={() => {
+                  router.push(`/restaurant/order/${item.id}`);
+                }}
+              />
+            </div>
           );
         });
       })}
--- src/view/restaurantOrderPrevious/component/RestaurantOrderPreviousList.tsx
+++ src/view/restaurantOrderPrevious/component/RestaurantOrderPreviousList.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import RestaurantOrderPreviousCard from "view/restaurantOrderPrevious/component/restaurantOrderPreviousCard/RestaurantOrderPreviousCard";
 import {useRestaurantOrderPreviousData} from "view/restaurantOrderPrevious/component/context/RestaurantOrderPreviousDataProvider";
 import {
   setRestaurantOrderPreviousReceiptData,
   useRestaurantOrderPreviousReceiptAction,
 } from "view/restaurantOrderPrevious/component/context/RestaurantOrderPreviousReceiptProvider";
+import {useInView} from "react-intersection-observer";
 
 function RestaurantOrderPreviousList() {
   const {data, isLoading} = useRestaurantOrderPreviousData();
@@ -20,31 +21,42 @@ function RestaurantOrderPreviousList() {
 
 function RestaurantOrderPreviousListShow() {
   const dispatch = useRestaurantOrderPreviousReceiptAction();
-  const {data} = useRestaurantOrderPreviousData();
+  const {data, fetchNextPage} = useRestaurantOrderPreviousData();
+  const {ref, inView} = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   return (
     <>
-      {data?.pages.map((value) => {
-        return value.orders.map((item) => {
+      {data?.pages.map((value, index, array) => {
+        return value.orders.map((item, idx, arr) => {
+          const condition = array.length - 1 === index && arr.length - 1 === idx;
+          const tmpRef = condition ? ref : null;
           return (
-            <RestaurantOrderPreviousCard
-              key={item.id}
-              id={item.id.toString()}
-              title={item.vendor.name}
-              receiptNumber={item.id}
-              image={item.vendor.logo}
-              deliveryTitle={item.address.title || ""}
-              coin={15}
-              status={item.orderstatus}
-              date={item.created_at}
-              orders={item.productKinds}
-              hasRate
-              totalPrice={Math.round(item.topayprice / 10)}
-              onClickReceipt={() => {
-                dispatch(setRestaurantOrderPreviousReceiptData());
-              }}
-              onClickReOrder={() => {}}
-            />
+            <div key={item.id} ref={tmpRef}>
+              <RestaurantOrderPreviousCard
+                key={item.id}
+                id={item.id.toString()}
+                title={item.vendor.name}
+                receiptNumber={item.id}
+                image={item.vendor.logo}
+                deliveryTitle={item.address.title || ""}
+                coin={15}
+                status={item.orderstatus}
+                date={item.created_at}
+                orders={item.productKinds}
+                hasRate
+                totalPrice={Math.round(item.topayprice / 10)}
+                onClickReceipt={() => {
+                  dispatch(setRestaurantOrderPreviousReceiptData());
+                }}
+                onClickReOrder={() => {}}
+              />
+            </div>
           );
         });
       })}

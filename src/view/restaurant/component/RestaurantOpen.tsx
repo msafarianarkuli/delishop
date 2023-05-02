99c507532b623721c3bdf0f3da56500bba87e480
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-05-02 20:58:33 +0330

    remove useless text

--- src/view/restaurant/component/RestaurantOpen.tsx
+++ src/view/restaurant/component/RestaurantOpen.tsx
@@ -1,5 +1,6 @@
 function RestaurantOpen() {
-  return <div className="font-medium text-[15px]">17 رستوران باز</div>;
+  // return <div className="font-medium text-[15px]">17 رستوران باز</div>;
+  return <div className="font-medium text-[15px]"></div>;
 }
 
 export default RestaurantOpen;
--- src/view/supermarket/component/SupermarketAround.tsx
+++ src/view/supermarket/component/SupermarketAround.tsx
@@ -3,7 +3,8 @@ import React from "react";
 function SupermarketAround() {
   return (
     <>
-      <div className="font-medium text-[15px] text-iconColor">17 سوپرمارکت اطراف شما</div>
+      {/*<div className="font-medium text-[15px] text-iconColor">17 سوپرمارکت اطراف شما</div>*/}
+      <div className="font-medium text-[15px] text-iconColor"></div>
     </>
   );
 }

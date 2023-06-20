f05e3987b20c6a304904213321d206453f941c9a
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2023-06-20 15:29:43 +0330

    convert supermarket to supermarket_old

--- src/view/supermarket/component/SupermarketSearch.tsx
+++ src/view/supermarket/component/SupermarketSearch.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function SupermarketSearch() {
   return (
     <Link
-      href="/supermarket/search"
+      href="/supermarket_old/search"
       className="flex items-center w-full border-0 rounded-full h-[55px] inner_box text-iconColor px-5 mb-5"
     >
       <IconSearch className="w-7 h-auto" />

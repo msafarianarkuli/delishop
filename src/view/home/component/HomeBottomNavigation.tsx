ede54d58fea3006bec69eac6f6c51363e65e3e21
maryam <m.safari.anarkuli@gmail.com>
2023-07-19 10:26:17 +0430

    add game 1  (aa) link to homebottomnavigation

--- src/view/home/component/HomeBottomNavigation.tsx
+++ src/view/home/component/HomeBottomNavigation.tsx
@@ -3,12 +3,13 @@ import message from "assets/images/homeMessage.svg";
 import divar from "assets/images/homeDivar.svg";
 import game1 from "assets/images/homeGame.svg";
 import game2 from "assets/images/homeGame2.svg";
+import Link from "next/link";
 
 const data = [
-  {title: "پیام ها", image: message.src},
-  {title: "دیوار", image: divar.src},
-  {title: "بازی 1", image: game1.src},
-  {title: "بازی 2", image: game2.src},
+  {title: "پیام ها", link: "", image: message.src},
+  {title: "دیوار", link: "", image: divar.src},
+  {title: "بازی 1", link: "https://aa.app.delishop.me/", image: game1.src},
+  {title: "بازی 2", link: "", image: game2.src},
 ];
 
 function HomeBottomNavigation() {
@@ -17,7 +18,12 @@ function HomeBottomNavigation() {
       <div className={styles.home_bottom_navigation}>
         <div className="flex w-full items-center justify-between mobile:mx-[30px] mx-3">
           {data.map((item, index) => {
-            return (
+            return item.title === "بازی 1" ? (
+              <Link href={item.link} target="_blank" key={index} className="h-[78px] w-full ml-2 last:ml-0">
+                <img src={item.image} alt={item.title} className="w-auto h-auto rounded-[6px] mx-auto" />
+                <div className="text-[13px] text-center mt-1">{item.title}</div>
+              </Link>
+            ) : (
               <div key={index} className="h-[78px] w-full ml-2 last:ml-0">
                 <img src={item.image} alt={item.title} className="w-auto h-auto rounded-[6px] mx-auto" />
                 <div className="text-[13px] text-center mt-1">{item.title}</div>
